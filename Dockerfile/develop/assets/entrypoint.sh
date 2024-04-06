1c6f492b70fa18366e94d332e0f3c7a21a405ae2
uijeong.jeong <uijeong.jeong@navercorp.com>
2024-04-07 02:25:44 +0900

    refactor: auth with jwt on github action

--- Dockerfile/develop/assets/entrypoint.sh
+++ Dockerfile/develop/assets/entrypoint.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 pem_file_path=${PEM_FILE_PATH***REMOVED***
-
-pip3 install jwt
-jwt_token=$(python3 assets/generate_jwt_token.py ${PEM_FILE_PATH***REMOVED*** ${APP_ID***REMOVED******REMOVED***
+jwt_token=$(python3 generate_jwt_token.py ${PEM_FILE_PATH***REMOVED*** ${APP_ID***REMOVED******REMOVED***
 
 registration_url="https://api.github.com/app/installations/${APP_ID***REMOVED***/access_tokens"
 echo "Requesting registration URL at '${registration_url***REMOVED***'"
 
-payload=$(curl --request POST \
---url ${registration_url***REMOVED*** \
---header "Accept: application/vnd.github+json" \
---header "Authorization: Bearer ${jwt_token***REMOVED***" \
---header "X-GitHub-Api-Version: 2022-11-28"***REMOVED***
+payload=$(curl -H "Authorization: Bearer ${jwt_token***REMOVED***" \
+-H "Accept: application/vnd.github.v3+json" \
+https://api.github.com/app/installations***REMOVED***
+installation_id=$(echo $payload | jq '.[0***REMOVED***.id'***REMOVED***
+
+payload=$(curl -X POST -H "Authorization: Bearer ${jwt_token***REMOVED***" \
+-H "Accept: application/vnd.github+json" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+https://api.github.com/app/installations/${installation_id***REMOVED***/access_tokens***REMOVED***
+
+registration_token=$(echo $payload | jq .token --raw-output***REMOVED***
+
+payload=$(curl -L \
+-X POST \
+-H "Accept: application/vnd.github+json" \
+-H "Authorization: token ${registration_token***REMOVED***" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+https://api.github.com/repos/subworkers/ojitong/actions/runners/registration-token***REMOVED***
+
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output***REMOVED***
 
 ./config.sh \
@@ -31,5 +43,4 @@ trap 'remove; exit 130' INT
 trap 'remove; exit 143' TERM
 
 ./run.sh "$*" &
-
 wait $!
