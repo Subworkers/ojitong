d16343a2892db0bb7b01d45e71dfdc9c4f0fe803
Uijeong97 <uiui_97@naver.com>
2024-07-06 11:49:30 +0000

    feat: fill publish_date null

--- workflow/data_preparation/news_scraper/data_enrichment.py
+++ workflow/data_preparation/news_scraper/data_enrichment.py
@@ -1,5 +1,6 @@
 # 3***REMOVED*** 결측 혹은 오류 데이터 보강
-
+import re
+from dateutil import parser
 import pandas as pd
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -40,7 +41,6 @@ def update_article_texts(df, driver***REMOVED***:
     return df
 
 def update_kbs_titles(df, driver***REMOVED***:
-
     for index, row in df.iterrows(***REMOVED***:
         if row['title'***REMOVED*** == 'KBS 뉴스':
             try:
@@ -53,3 +53,36 @@ def update_kbs_titles(df, driver***REMOVED***:
             except (NoSuchElementException, TimeoutException, WebDriverException***REMOVED*** as e:
                 print(f"An error occurred for URL {row['url'***REMOVED******REMOVED***: {e***REMOVED***"***REMOVED***
     return df
+
+# publish_date의 null 값 처리
+def fill_missing_publish_dates(df, driver***REMOVED***:
+    date_classes = ['news-date', 'date-repoter', 'article_info', 'article_byline', 'dates', 'dateFont', 'article_date'***REMOVED***
+    for index, row in df.iterrows(***REMOVED***:
+        if pd.isna(row['publish_date'***REMOVED******REMOVED***:
+            url = row['url'***REMOVED***
+            try:
+                driver.get(url***REMOVED***
+            except TimeoutException:
+                print(f"Timeout while loading {url***REMOVED***"***REMOVED***
+                continue
+            
+            publish_date = None
+            for date_class in date_classes:
+                date_elements = driver.find_elements(By.CLASS_NAME, date_class***REMOVED***
+                for date_element in date_elements:
+                    date_text = date_element.text
+                    numbers = re.findall(r'\d+', date_text***REMOVED***
+                    if len(numbers***REMOVED*** >= 3:
+                        publish_date = f"{numbers[0***REMOVED******REMOVED***-{numbers[1***REMOVED***.zfill(2***REMOVED******REMOVED***-{numbers[2***REMOVED***.zfill(2***REMOVED******REMOVED***"
+                        break
+                if publish_date:
+                    break
+            
+            if publish_date:
+                try:
+                    parsed_date = parser.parse(publish_date***REMOVED***
+                    df.at[index, 'publish_date'***REMOVED*** = parsed_date
+                except ValueError as e:
+                    print(f"Error parsing date from {url***REMOVED***: {e***REMOVED***"***REMOVED***
+    return df
+
