d057b0f6c06dc63c7857153e28daa5b3a43fe6fa
Uijeong97 <uiui_97@naver.com>
2024-07-07 15:17:07 +0900

    feat(workflow): utilize knowledge selection task

---
+++ data/tasks/base_task.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from langchain_openai import ChatOpenAI
+
+class BaseTask(ABC):
+    def __init__(self):
+        self.llm = self._build_llm()
+        self.prompt_template = self._build_prompt()
+        self.parser = self._build_parser()
+        self.chain = self._build_chain()
+
+    def _build_llm(self):
+        import os
+        os.environ["OPENAI_API_KEY"] = "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"
+        return ChatOpenAI(model="gpt-4o")
+    
+    @abstractmethod
+    def _build_prompt(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _build_parser(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _build_chain(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def execute(self, *args, **kwargs):
+        raise NotImplementedError()
+
+
+class WritingTask(BaseTask, ABC):
+    def __init__(self, category):
+        self.category = category
+        self.prompt_template = self._build_prompt()
+        self.parser = self._build_parser()
+        self.chain = self._build_chain()
+
+    @abstractmethod
+    def _build_prompt(self):
+        pass
+
+    @abstractmethod
+    def _build_parser(self):
+        pass
+
+    @abstractmethod
+    def _build_chain(self):
+        pass
+
+    @abstractmethod
+    def execute(self, content):
+        pass
