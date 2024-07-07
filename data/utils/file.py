4d60cc7550268117c5bd247b81dd6394b479aedd
Uijeong97 <uiui_97@naver.com>
2024-07-07 15:06:02 +0900

    refactor(crawling): generate s3 path util

---
+++ data/utils/file.py
@@ -0,0 +1,33 @@
+import json
+import jsonlines
+from pathlib import Path
+
+from data import DATA_BASE_PATH
+from data.const import POST_TYPE, DATA_TYPE
+
+RESOURCES_PATH = Path(DATA_BASE_PATH) / "bench"
+
+def save_bench_data_jsonl_by_types(post_type: POST_TYPE, data_type: DATA_TYPE, samples: list):
+    filename = set_filename_by_data_type(data_type)
+    file_path = RESOURCES_PATH / post_type.value / filename
+    write_samples_by_line(file_path, samples)
+
+def set_filename_by_data_type(data_type: DATA_TYPE):
+    filename = f"{data_type.value}.jsonl"
+    return filename
+
+def write_samples_by_line(file_path, samples: list):
+    with open(file_path, "w", encoding="utf-8") as f:
+        for data in samples:
+            json.dump(data, f, ensure_ascii=False)
+            f.write("\n")
+
+def read_samples_by_line(file):
+    file_path = str(RESOURCES_PATH.joinpath(file))
+    with jsonlines.open(file_path) as f:
+        for line in f.iter():
+            yield json.loads(line)
+
+def generate_s3_path(bucket_name="ojitong", prefix="news_data", extension=".csv", today_datetime: datetime=datetime.today()):
+    timestamp = today_datetime.strftime("%Y%m%d_%H%M%S")
+    return f"s3://{bucket_name}/{deploy_phase}/{prefix}/{timestamp}{extension}"
