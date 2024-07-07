1d1dabdaa8bf976131270695715d53077cc71709
Uijeong97 <uiui_97@naver.com>
2024-07-07 15:36:53 +0900

    fix(workflow): import error

--- data/utils/file.py
+++ data/utils/file.py
@@ -1,18 +1,18 @@
 import json
 import jsonlines
+from datetime import datetime
 from pathlib import Path
-
 from data import DATA_BASE_PATH
-from data.const import POST_TYPE, DATA_TYPE
+from data.const import PostType, DataType
 
 RESOURCES_PATH = Path(DATA_BASE_PATH***REMOVED*** / "bench"
 
-def save_bench_data_jsonl_by_types(post_type: POST_TYPE, data_type: DATA_TYPE, samples: list***REMOVED***:
+def save_bench_data_jsonl_by_types(post_type: PostType, data_type: DataType, samples: list***REMOVED***:
     filename = set_filename_by_data_type(data_type***REMOVED***
     file_path = RESOURCES_PATH / post_type.value / filename
     write_samples_by_line(file_path, samples***REMOVED***
 
-def set_filename_by_data_type(data_type: DATA_TYPE***REMOVED***:
+def set_filename_by_data_type(data_type: DataType***REMOVED***:
     filename = f"{data_type.value***REMOVED***.jsonl"
     return filename
 
@@ -28,6 +28,12 @@ def read_samples_by_line(file***REMOVED***:
         for line in f.iter(***REMOVED***:
             yield json.loads(line***REMOVED***
 
-def generate_s3_path(bucket_name="ojitong", prefix="news_data", extension=".csv", today_datetime: datetime=datetime.today(***REMOVED******REMOVED***:
+def generate_s3_path(
+    bucket_name="ojitong",
+    deploy_phase="local",
+    prefix="news_data",
+    extension=".csv",
+    today_datetime: datetime=datetime.today(***REMOVED***
+***REMOVED***:
     timestamp = today_datetime.strftime("%Y%m%d_%H%M%S"***REMOVED***
     return f"s3://{bucket_name***REMOVED***/{deploy_phase***REMOVED***/{prefix***REMOVED***/{timestamp***REMOVED***{extension***REMOVED***"
