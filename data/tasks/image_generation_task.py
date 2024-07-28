1531e7f129d14678abf3cbc04be3650a7f1ada7c
Uijeong97 <uiui_97@naver.com>
2024-07-28 20:58:43 +0900

    feat(image): image generation task

---
+++ data/tasks/image_generation_task.py
@@ -0,0 +1,32 @@
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import OpenAI
+from data.tasks.base_task import BaseTask
+from data.templates.image_generation_template import ImageTemplate
+from data.processors import S3ImageUploader
+
+class ImageGenerationTask(BaseTask):
+    def __init__(self):
+        super().__init__()
+        self.dalle_wrapper = DallEAPIWrapper(
+            model="dall-e-3",
+        )
+        self.s3_image_uploader = S3ImageUploader()
+
+    def _build_llm(self):
+        return OpenAI()
+
+    def _build_template(self):
+        prompt = ImageTemplate().prompt
+        return ChatPromptTemplate.from_template(prompt)
+    
+    def _build_chain(self):
+        return self.prompt_template | self.llm
+
+    def _build_parser(self):
+        pass
+
+    def execute(self, content):
+        query = self.chain.invoke({"text": content})
+        image_url = self.dalle_wrapper.run(query)
+        return image_url
