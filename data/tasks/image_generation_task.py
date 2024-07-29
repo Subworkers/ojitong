c7c9aa622f70a6ba695ca1c53859e7cc98c2e556
Uijeong97 <uiui_97@naver.com>
2024-07-29 20:18:21 +0900

    chore: remove unused object

--- data/tasks/image_generation_task.py
+++ data/tasks/image_generation_task.py
@@ -3,7 +3,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import OpenAI
 from data.tasks.base_task import BaseTask
 from data.templates.image_generation_template import ImageTemplate
-from data.processors import S3ImageUploader
 
 class ImageGenerationTask(BaseTask):
     def __init__(self):
@@ -11,7 +10,6 @@ class ImageGenerationTask(BaseTask):
         self.dalle_wrapper = DallEAPIWrapper(
             model="dall-e-3",
         )
-        self.s3_image_uploader = S3ImageUploader()
 
     def _build_llm(self):
         return OpenAI()
