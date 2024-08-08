5dfd0ead38718587130b8184d2d427420a6bed63
Uijeong97 <uiui_97@naver.com>
2024-08-08 21:44:16 +0900

    fix: datetime parser

---
+++ data/utils/date.py
@@ -0,0 +1,5 @@
+from dateutil import parser
+
+# datetime parser
+def parse_any_datetime_format(date_str***REMOVED***:
+    return parser.parse(date_str***REMOVED***.strftime('%Y-%m-%d %H:%M:%S'***REMOVED***
